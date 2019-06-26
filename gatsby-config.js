687d5a89050d8257e79926df0dfc44237d27ad1c
Ed Summers <ehs@pobox.com>
2019-06-26 16:21:00 -0400

    use react-helmet to update <head>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'MITH',
     description: 'Maryland Institute for Technology in the Humanities'
-  }
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`
+  ]
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,18 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default props => {
   return (
-    <html>
-      <head>
+    <>
+      <Helmet>
         <title>MITH: {props.title}</title>
         <link rel="shortcut icon" href="/images/favicon.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-      </head>
+      </Helmet>
       <body>{props.children}</body>
-    </html>
+    </>
   )
 }
